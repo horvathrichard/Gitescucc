de2faa26eb32865ec7b41e0a19ff7cf3e17d53ae
ricso <ricso@RICSZ>
2024-04-11 11:34:50 +0200

    Add project files.

---
+++ Gitescucc/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gitescucc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
